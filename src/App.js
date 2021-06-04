4e4968579000bee949676533bb325b6591994db7
Deveshb15 <deveshb583@gmail.com>
2021-06-04 19:37:33 +0530

    Lazy loading

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, {Component} from 'react'
+import React, {Component, lazy, Suspense} from 'react'
 import './App.css';
 
 import Page1 from './components/Page1'
-// import Page2 from './components/Page2'
-// import Page3 from './components/Page3'
+const AsyncPage2 = lazy(() => import('./components/Page2'))
+const AsyncPage3 = lazy(() => import('./components/Page3'))
 
-import AsyncComponent from './components/AsyncComponent'
+// import AsyncComponent from './components/AsyncComponent'
 
 class App extends Component{
 
@@ -17,7 +17,7 @@ class App extends Component{
   onRouteChange = (route) => {
     // No-code splitting
     this.setState({ route: route });
-    
+
     // Code Splitting
     // if(route === 'page1') {
     //   this.setState({route: route})
@@ -46,15 +46,23 @@ class App extends Component{
   render() {
 
   // No code Splitting 
-  // if(route === 'page1'){
-  //   return <Page1 onRouteChange={onRouteChange} />
-  // }
-  // else if(route === 'page2'){
-  //   return  <Page2 onRouteChange={onRouteChange} />
-  // }
-  // else if(route === 'page3'){
-  //   return <Page3 onRouteChange={onRouteChange} />
-  // }
+  if(this.state.route === 'page1'){
+    return <Page1 onRouteChange={this.onRouteChange} />
+  }
+  else if(this.state.route === 'page2'){
+    return  (
+      <Suspense fallback={<div>Loading...</div>}>
+        <AsyncPage2 onRouteChange={this.onRouteChange} />
+      </Suspense>
+    )
+  }
+  else if(this.state.route === 'page3'){
+    return  (
+      <Suspense fallback={<div>Loading...</div>}>
+        <AsyncPage3 onRouteChange={this.onRouteChange} />
+      </Suspense>
+    )
+  }
 
   // Code Splitting 
     // if (this.state.route === 'page1') {
@@ -64,17 +72,17 @@ class App extends Component{
     // }
 
     // Async Component
-    if(this.state.route === 'page1'){
-      return <Page1 onRouteChange={this.onRouteChange} />
-    }
-    else if(this.state.route === 'page2'){
-      const AsyncPage2 = AsyncComponent(() => import('./components/Page2'))
-      return  <AsyncPage2 onRouteChange={this.onRouteChange} />
-    }
-    else if(this.state.route === 'page3'){
-      const AsyncPage3 = AsyncComponent(() => import('./components/Page3'))
-      return  <AsyncPage3 onRouteChange={this.onRouteChange} />
-    }
+    // if(this.state.route === 'page1'){
+    //   return <Page1 onRouteChange={this.onRouteChange} />
+    // }
+    // else if(this.state.route === 'page2'){
+    //   const AsyncPage2 = AsyncComponent(() => import('./components/Page2'))
+    //   return  <AsyncPage2 onRouteChange={this.onRouteChange} />
+    // }
+    // else if(this.state.route === 'page3'){
+    //   const AsyncPage3 = AsyncComponent(() => import('./components/Page3'))
+    //   return  <AsyncPage3 onRouteChange={this.onRouteChange} />
+    // }
 
   }
 
