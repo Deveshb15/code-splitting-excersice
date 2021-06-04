7c7ca8125b0cfe4718fd2e93349a960d2024adf0
Deveshb15 <deveshb583@gmail.com>
2021-06-04 20:03:53 +0530

    Code Splitting

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 
 // No code splitting
 import Page1 from './components/Page1'
-import Page2 from './components/Page2'
-import Page3 from './components/Page3'
+// import Page2 from './components/Page2'
+// import Page3 from './components/Page3'
 
 
 
@@ -17,18 +17,49 @@ class App extends Component{
 
   onRouteChange = (route) => {
     // No-code splitting
-    this.setState({ route: route });
+    // this.setState({ route: route });
+
+    // Code Splitting
+    if(route === 'page1') {
+      this.setState({route: route})
+    }
+    else if(route === 'page2') {
+      import('./components/Page2')
+        .then((Page2) => {
+          console.log(Page2);
+          this.setState({ route: route, component: Page2.default })
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+    else {
+      import('./components/Page3')
+        .then(Page3 => {
+          this.setState({route: route, component: Page3.default})
+        })
+        .catch(err => {
+          console.log(err);
+        })
+    }
   }
 
   render() {
 
   // No code Splitting 
+    // if (this.state.route === 'page1') {
+    //   return <Page1 onRouteChange={this.onRouteChange} />
+    // } else if (this.state.route === 'page2') {
+    //   return <Page2 onRouteChange={this.onRouteChange} />
+    // } else {
+    //   return <Page3 onRouteChange={this.onRouteChange} />
+    // }
+
+    //  Code Splitting 
     if (this.state.route === 'page1') {
       return <Page1 onRouteChange={this.onRouteChange} />
-    } else if (this.state.route === 'page2') {
-      return <Page2 onRouteChange={this.onRouteChange} />
     } else {
-      return <Page3 onRouteChange={this.onRouteChange} />
+      return <this.state.component onRouteChange={this.onRouteChange} />
     }
 
   }
